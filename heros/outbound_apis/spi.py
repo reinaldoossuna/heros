fcce8b8dfe468953413d76390df51ff15d1dc6f0
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2024-12-20 18:57:37 -0400

    better handle error and typing

--- heros/outbound_apis/spi.py
+++ heros/outbound_apis/spi.py
@@ -1,9 +1,10 @@
 import logging
 
-import pandas as pd
 import aiohttp
+from typing import Optional, List
+from pydantic import ValidationError
 
-from heros.types.sensordata import sensors_data_list
+from heros.types.sensordata import sensors_data_list, SensorData
 
 API_URL = "https://leituras.spi.engtecnologia.com/api/"
 LOGIN_URL = API_URL + "usuarios/login"
@@ -12,12 +13,11 @@ DATA_URL = API_URL + "leituras/listar_leituras_por_idempresa"
 LOGGER = logging.getLogger(__name__)
 
 
-async def login(username: str, password: str):
+async def login(username: str, password: str) -> Optional[aiohttp.ClientSession]:
     LOGGER.info(f"Logging in as {username}")
     session = aiohttp.ClientSession()
-    async with session.post(
-        LOGIN_URL, json={"email_usuario": username, "senha": password}
-    ) as r:
+    cred = {"email_usuario": username, "senha": password}
+    async with session.post(LOGIN_URL, json=cred) as r:
         if r.ok:
             resp = await r.json()
             LOGGER.debug(f"got from the server: {resp}")
@@ -25,16 +25,23 @@ async def login(username: str, password: str):
             session.headers["Authorization"] = f"Bearer {token}"
             return session
         else:
-            return r.reason()
+            LOGGER.error(f"Failed to login as {username}, reason: {r.reason}")
+            return None
 
 
-async def request_data(session):
+async def request_data(session) -> Optional[List[SensorData]]:
     LOGGER.info(f"Requesting data from {API_URL}")
     async with session.get(DATA_URL) as r:
         if r.ok:
             resp = await r.json()
             LOGGER.debug(f"got from the server: {resp}")
             raw = resp["result"]
-            return sensors_data_list.validate_python(raw)
+            try:
+                return sensors_data_list.validate_python(raw)
+            except ValidationError as err:
+                LOGGER.error(f"Failed to parse data.\n{err}")
+                return None
+
         else:
-            return r.reason()
+            LOGGER.error(f"Failed to get data, reason: {r.reason}")
+            return None
