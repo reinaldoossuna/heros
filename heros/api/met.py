58620ab005f1dd95b02e57ffe37186643ab811d1
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2024-12-24 10:23:14 -0400

    use new data type in the api

--- heros/api/met.py
+++ heros/api/met.py
@@ -5,6 +5,7 @@ from aiohttp import web
 
 import heros.db_access.sql as sql
 import heros.outbound_apis.wxt as wxt
+from heros.types.wxtdata import RequestsFields
 
 
 async def get_data(request):
@@ -14,6 +15,8 @@ async def get_data(request):
         dicts = [d.model_dump_json() for d in data]
         return web.json_response(dicts)
 
+def to_tuple(d):
+    return tuple(d.values())
 
 async def update_sensores(request):
     config = request.app["noaa_cfg"]
@@ -22,12 +25,13 @@ async def update_sensores(request):
     conn = await pool.acquire()
     last_day = await sql.last_day_met(conn)
 
-    req = wxt.RequestsFields(StartDt=last_day)
+    req = RequestsFields(start_date=last_day)
+    datas = await wxt.request_data(req, config["user"], config["password"])
 
-    raw_jsons = await wxt.request_data(req, config["user"], config["password"])
-    clean_json = map(wxt.parse_one_json, raw_jsons)
 
-    await sql.insert_wxtdata(conn, chain.from_iterable(clean_json))
+    tuples = map(to_tuple, filter(lambda d: d != None, chain.from_iterable(datas)))
+
+    await sql.insert_wxtdata(conn, tuples)
     await conn.close()
 
     return web.Response()
