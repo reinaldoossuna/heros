d6bb6ceddcc3685d12ee39f89e72fbf093448cc7
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2024-12-31 15:43:25 -0400

    use new db handles

--- heros/api/metereologico.py
+++ heros/api/metereologico.py
@@ -3,30 +3,30 @@ from itertools import chain
 
 from aiohttp import web
 
-import heros.db_access.sql as sql
+import heros.db_access.meterologico as met
 import heros.outbound_apis.noaa as noaa
+from heros.config import settings
+from heros.db_access import pool
 
 
 async def get_data(request):
-    pool = request.app["pool"]
     start = request.rel_url.query.get("start", None)
     end = request.rel_url.query.get("end", None)
 
     start = datetime.strptime(start, "%d%m%Y") if start else None
     end = datetime.strptime(end, "%d%m%Y") if end else None
-    async with pool.acquire() as conn:
-        data = await sql.get_met_data(conn, start, end)
+    with pool.connection() as conn:
+        data = met.get_data(conn, start, end)
         dicts = [d.model_dump_json() for d in data]
         return web.json_response(dicts)
 
 
 async def update_sensores(request):
-    config = request.app["settings"].noaa
-    pool = request.app["pool"]
+    config = settings.noaa
 
-    conn = await pool.acquire()
-    last_day = await sql.last_day_met(conn)
-    last_day = last_day if last_day is not None else datetime.fromtimestamp(0)
+    with pool.connection() as conn:
+        last_day = met.last_timestamp(conn)
+        last_day = last_day if last_day is not None else datetime.fromtimestamp(0)
 
     noaamsgs = await noaa.request_data(config.user, config.password, start_date=last_day)
     if noaamsgs is None:
@@ -34,14 +34,14 @@ async def update_sensores(request):
 
     datas = chain.from_iterable(map(lambda m: m.model_dump(), noaamsgs))
 
-    await sql.insert_wxtdata(conn, datas)
-    await conn.close()
+    with pool.connection() as conn:
+        met.insert_update_data(conn, datas)
 
     return web.Response()
 
 
 async def can_login(request: web.Request) -> web.Response:
-    config = request.app["settings"].noaa
+    config = settings.noaa
     s = await noaa.login(config.user, config.password)
 
     if s is not None:
