285aaa857c4f50e7b4a659d765dca930d1072329
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2024-12-31 16:49:47 -0400

    dont bass settings as context

--- heros/server.py
+++ heros/server.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import heros.api.linigrafos as linigrafos
 import heros.api.metereologico as met
 from heros.logging import setup_logging
-import heros.config as cfg
+from heros.config import settings
 
 load_dotenv()
 
@@ -16,9 +16,6 @@ LOGGER = logging.getLogger(__name__)
 def create_app():
     app = web.Application()
 
-    settings = cfg.Settings()
-    app["settings"] = settings
-
     app.add_routes(
         [
             web.get("/metereologico", met.get_data),
@@ -27,7 +24,7 @@ def create_app():
             # web.get("/met/updatepassword", met.update_password),
             web.get("/linigrafos", linigrafos.get_data),
             web.get("/linigrafos/update", linigrafos.update_sensores),
-            web.get("/linigrafos/last_update", linigrafos.last_update),
+            web.get("/linigrafos/sensors/lastupdate", linigrafos.sensors_lastupdate),
             web.get("/linigrafos/update_location", linigrafos.update_location),
         ]
     )
@@ -39,7 +36,7 @@ def main_server():
     setup_logging()
     LOGGER.info("Starting server ...")
     app = create_app()
-    web.run_app(app, port=app["settings"].port)
+    web.run_app(app, port=settings.port)
 
 
 if __name__ == "__main__":
