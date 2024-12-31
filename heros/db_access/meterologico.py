b3fc56431c1a471d9e9f0c36704ceca1cf78e656
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2024-12-31 15:42:35 -0400

    new met handles using psycopg

---
+++ heros/db_access/meterologico.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+
+from psycopg import Connection
+from psycopg.rows import class_row
+from typing import List, Optional, Iterable
+
+from heros.types.db.metereologico import MetereologicoData
+
+
+def get_data(
+    conn: Connection, start: Optional[datetime] = None, end: Optional[datetime] = None
+) -> List[MetereologicoData]:
+    with conn.cursor(row_factory=class_row(MetereologicoData)) as cur:
+        cur.execute(
+            """
+        SELECT *
+        FROM hydronet.public.dados_met ds
+        WHERE
+        ds."DATA" BETWEEN
+                    COALESCE(%s, to_timestamp(0)::date)
+                    AND COALESCE(%s, CURRENT_TIMESTAMP);
+        """,
+            (start, end),
+        )
+        return cur.fetchall()
+
+
+def last_timestamp(conn: Connection) -> Optional[datetime]:
+    with conn.cursor(row_factory=class_row(datetime)) as cur:
+        cur.execute("""
+        SELECT dm."Data"
+        FROM hydronet.public.dados_met dm
+        ORDER BY dm."Data" DESC
+        LIMIT 1;
+        """)
+        return cur.fetchone()
+
+
+# TODO: Should recieve MetereologicoData
+def insert_update_data(conn: Connection, datas: Iterable[tuple]):
+    with conn.cursor() as cur:
+        cur.executemany(
+            """
+        INSERT
+        INTO
+        hydronet.public.dados_met as t
+       (
+            "Data",
+            "Pressão atmosférica (bar)",
+            "Temperatura do ar (°C)",
+            "Umidade relativa do ar (%%)",
+            "Precipitação (mm)",
+            "Velocidade do vento (m/s)",
+            "Direção do vento (˚)",
+            "Bateria (v)"
+       )
+        VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+        ON
+        CONFLICT ("Data")
+        DO
+        UPDATE
+        SET
+            (
+                "Pressão atmosférica (bar)",
+                "Temperatura do ar (°C)",
+                "Umidade relativa do ar (%%)",
+                "Precipitação (mm)",
+                "Velocidade do vento (m/s)",
+                "Direção do vento (˚)",
+                "Bateria (v)"
+            ) =
+        (COALESCE(EXCLUDED."Pressão atmosférica (bar)",
+        t."Pressão atmosférica (bar)"),
+        COALESCE(EXCLUDED."Temperatura do ar (°C)",
+        t."Temperatura do ar (°C)"),
+        COALESCE(EXCLUDED."Umidade relativa do ar (%%)",
+        t."Umidade relativa do ar (%%)"),
+        COALESCE(EXCLUDED."Precipitação (mm)",
+        t."Precipitação (mm)"),
+        COALESCE(EXCLUDED."Velocidade do vento (m/s)",
+        t."Velocidade do vento (m/s)"),
+        COALESCE(EXCLUDED."Direção do vento (˚)",
+        t."Direção do vento (˚)"),
+        COALESCE(EXCLUDED."Bateria (v)",
+        t."Bateria (v)"));
+        """,
+            datas,
+        )
+        conn.commit()
