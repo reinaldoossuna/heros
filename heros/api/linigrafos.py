89f5223fc443387029c68a82e9be0c72b62a0b4e
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2024-12-31 16:48:57 -0400

    use the new linigrafos db handles

--- heros/api/linigrafos.py
+++ heros/api/linigrafos.py
@@ -2,13 +2,13 @@ from aiohttp import web
 from datetime import datetime
 
 from heros.logging import LOGGER
+from heros.config import settings
+from heros.db_access import pool
+import heros.db_access.linigrafos as lini
 import heros.outbound_apis.engtec as engtec
-import heros.db_access.sql as sql
-from heros.types.localizacao import SensorLocalizacao
 
 
 async def get_data(request: web.Request):
-    pool = request.app["pool"]
     start = request.rel_url.query.get("start", None)
     end = request.rel_url.query.get("end", None)
 
@@ -18,53 +18,31 @@ async def get_data(request: web.Request):
     LOGGER.info(f"start: {start}")
     LOGGER.info(f"end: {end}")
 
-    async with pool.acquire() as conn:
-        data = await sql.get_sensor_data(conn, start, end)
-        dicts = [d.model_dump_json() for d in data]
-        return web.json_response(dicts)
+    with pool.connection() as conn:
+        data = lini.get_data(conn, start, end)
+        dumped = [d.model_dump_json() for d in data]
+        return web.json_response(dumped)
 
 
 async def update_sensores(request) -> web.Response:
-    config = request.app["settings"].engtec
-
-    session = await engtec.login(config.user, config.password)
-    if session is None:
+    datas = await engtec.request_data(settings.engtec)
+    if datas is None:
         return web.Response(
             status=400,
-            text="Failed to login",
+            text="Failed to get data from Engtec api",
         )
 
-    data = await engtec.request_data(session)
-    await session.close()
-
-    pool = request.app["pool"]
-    async with pool.acquire() as conn:
-        await sql.insert_sensordata(conn, data)
+    with pool.connection() as conn:
+        lini.insert_data(conn, datas)
     return web.Response()
 
 
-async def last_update(request):
-    pool = request.app["pool"]
-    async with pool.acquire() as conn:
-        data = await sql.get_lastupdate_sensors(conn)
+async def sensors_lastupdate(request):
+    with pool.connection() as conn:
+        data = lini.get_sensors_lastupdate(conn)
         dumped = [d.model_dump_json() for d in data]
         return web.json_response(dumped)
 
 
 async def update_location(request):
-    pool = request.app["pool"]
-    data = SensorLocalizacao(
-        p_name=request.rel_url.query["local"], s_mac=request.rel_url.query["mac"]
-    )
-    async with pool.acquire() as conn:
-        async with conn.transaction():
-            # TODO: query
-            await conn.execute(
-                """
-            INSERT INTO localizacao_sensores (p_name, s_mac)
-            VALUES ($1, $2);
-            """,
-                data.p_name,
-                data.s_mac,
-            )
-    return web.Response()
+    raise NotImplementedError
