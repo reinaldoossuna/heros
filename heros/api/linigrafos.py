4e455d98f245df2236e5e12870a06abbdcbb8b77
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2024-12-31 17:43:50 -0400

    add some logging in the linigrafos api

--- heros/api/linigrafos.py
+++ heros/api/linigrafos.py
@@ -21,26 +21,33 @@ async def get_data(request: web.Request):
     with pool.connection() as conn:
         data = lini.get_data(conn, start, end)
         dumped = [d.model_dump_json() for d in data]
+        LOGGER.info("Sending data")
         return web.json_response(dumped)
 
 
 async def update_sensores(request) -> web.Response:
+    LOGGER.info("Updating linigrafos data")
     datas = engtec.request_data(settings.engtec)
     if datas is None:
+        LOGGER.error("Updated failed")
         return web.Response(
             status=400,
             text="Failed to get data from Engtec api",
         )
 
+    LOGGER.info("Sending new data to db")
     with pool.connection() as conn:
         lini.insert_data(conn, datas)
+    LOGGER.info("Updated done")
     return web.Response()
 
 
 async def sensors_lastupdate(request):
+    LOGGER.info("Sensors lastupdate requested, getting data in db")
     with pool.connection() as conn:
         data = lini.get_sensors_lastupdate(conn)
         dumped = [d.model_dump_json() for d in data]
+        LOGGER.info("Sending data")
         return web.json_response(dumped)
 
 
