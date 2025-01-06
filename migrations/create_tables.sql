ab8d233d2f018bb43926f49b2eb264fd4b1b6413
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2025-01-06 17:23:22 -0400

    migrations file for the db
    notice the tables have changed names and
    that the wxt530 columns have changed too.
    
    there's the use of timescaledb's hypertable

---
+++ migrations/create_tables.sql
@@ -0,0 +1,96 @@
+-- WXT530 Table --
+
+CREATE TABLE IF NOT EXISTS wxt530 (
+	data timestamp NOT NULL,
+	pressao_atmosferica float4 NULL, -- Pressao atmosferica medida em bar
+	temperatura float4 NULL, -- Temperatura do ar em celciuls
+	umidade_ar float4 NULL, -- Umidade relativa do ar (%)
+	precipitacao float4 NULL, -- Precipitação (mm)
+	velocidade_vento float4 NULL, -- Velocidade do vento (m/s)
+	direcao_vento float4 NULL, -- Direção do vento (˚)
+	bateria float4 NULL, -- Bateria equipamento (V)
+
+	CONSTRAINT wxt530_pkey PRIMARY KEY ("data"),
+	CONSTRAINT wxt530_unique UNIQUE ("data")
+);
+-- Column comments
+
+COMMENT ON COLUMN wxt530.pressao_atmosferica IS 'Pressão atmosférica (bar)';
+COMMENT ON COLUMN wxt530.temperatura IS 'Temperatura do ar (°C)';
+COMMENT ON COLUMN wxt530.umidade_ar IS 'Umidade relativa do ar (%)';
+COMMENT ON COLUMN wxt530.precipitacao IS 'Precipitação (mm)';
+COMMENT ON COLUMN wxt530.velocidade_vento IS 'Velocidade do vento (m/s)';
+COMMENT ON COLUMN wxt530.direcao_vento IS 'Direção do vento (˚)';
+COMMENT ON COLUMN wxt530.bateria IS 'Bateria do aparelho (v)';
+
+-- Timescale hypertable
+SELECT create_hypertable('wxt530', by_range('data'), if_not_exists => TRUE);
+CREATE INDEX IF NOT EXISTS ix_wxt530_time ON wxt530 (data DESC);
+
+ALTER TABLE wxt530 SET (
+  timescaledb.compress
+);
+
+SELECT add_compression_policy('wxt530', compress_after => INTERVAL '365d', if_not_exists => TRUE);
+
+-- Linigrafos Table --
+
+CREATE TABLE IF NOT EXISTS posicoes (
+	nome bpchar(12) NOT NULL,
+	latitude float4 NULL,
+	longitude float4 NULL,
+	CONSTRAINT posicoes_pk PRIMARY KEY (nome)
+);
+
+
+INSERT INTO posicoes (nome,latitude,longitude) VALUES
+	 ('soter',-20.43788,-54.58097),
+	 ('hemosul',-20.46824,-54.60931),
+	 ('rq30',-20.4648,-54.59733),
+	 ('segredo',-20.4349,-54.62027),
+	 ('segredo2',-20.4588,-54.6219),
+	 ('bandeira',-20.50256,-54.60454)
+	ON CONFLICT DO NOTHING;
+
+
+CREATE TABLE IF NOT EXISTS linigrafos (
+	data_leitura timestamptz NOT NULL,
+	mac bpchar(12) NOT NULL,
+	valor_leitura float4 NOT NULL,
+	sub_id_disp bpchar(12) NULL,
+	canal int8 NOT NULL,
+
+	CONSTRAINT linigrafos_pkey PRIMARY KEY (mac, data_leitura),
+	FOREIGN KEY (sub_id_disp) REFERENCES posicoes(nome)
+);
+
+-- Timescale hypertable
+SELECT create_hypertable('linigrafos', by_range('data_leitura'), if_not_exists => TRUE);
+CREATE INDEX IF NOT EXISTS ix_linigrafos_time ON linigrafos (mac, data_leitura DESC);
+
+ALTER TABLE linigrafos SET (
+  timescaledb.compress,
+  timescaledb.compress_segmentby = 'mac'
+);
+SELECT add_compression_policy('linigrafos', compress_after => INTERVAL '365d', if_not_exists => TRUE);
+
+-- sub_id_disp nullify
+
+CREATE OR REPLACE FUNCTION posicao_or_null()
+RETURNS  TRIGGER
+LANGUAGE PLPGSQL
+AS
+$$
+BEGIN
+	IF (SELECT nome FROM posicoes WHERE posicoes.nome = NEW.sub_id_disp) IS NULL THEN
+	   NEW.sub_id_disp = NULL;
+	END IF;
+	RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE TRIGGER valid_posicao
+BEFORE INSERT
+ON linigrafos
+FOR EACH ROW
+EXECUTE FUNCTION posicao_or_null();
