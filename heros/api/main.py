f600fe6439cced757659074f3a8b010482717cae
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2025-01-08 15:36:22 -0400

    dummy healty endpoint

--- heros/api/main.py
+++ heros/api/main.py
@@ -6,3 +6,8 @@ api_router = APIRouter()
 api_router.include_router(linigrafos.router)
 api_router.include_router(metereologico.router)
 api_router.include_router(location.router)
+
+
+@api_router.get("/healty", status_code=200)
+def healty():
+    return "Ok"
