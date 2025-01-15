ec6aaee8cbc5ce081622724e13dfb32a066bcd25
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2025-01-15 18:13:30 -0400

    move location to db types

--- backend/heros/api/location.py
+++ backend/heros/api/location.py
@@ -4,7 +4,7 @@ from psycopg.rows import class_row
 
 from heros.db_access import pool
 from heros.logging import LOGGER
-from heros.types.location import LocationData
+from heros.types.db.location import LocationData
 
 router = APIRouter(prefix="/location", tags=["locations"])
 
