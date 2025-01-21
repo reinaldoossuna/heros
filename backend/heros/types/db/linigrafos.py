49a5caaad590cb249f4ff7e60a74309fb05e623d
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2025-01-21 12:46:01 -0400

    back: strip whitespace

--- backend/heros/types/db/linigrafos.py
+++ backend/heros/types/db/linigrafos.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class LinigrafoData(BaseModel):
     """
     Sensor in the db.
     """
+
+    model_config = ConfigDict(str_strip_whitespace=True)
 
     data_leitura: datetime
     mac: str
