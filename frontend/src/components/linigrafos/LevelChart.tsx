f872a4fd0ac1a9527bc45a09ec1ea64797d58043
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2025-03-13 14:32:46 -0400

    use the avg api to show linigrafos data

--- frontend/src/components/linigrafos/LevelChart.tsx
+++ frontend/src/components/linigrafos/LevelChart.tsx
@@ -1,36 +1,65 @@
 import moment from 'moment';
 import { XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, AreaChart, Area } from 'recharts';
-import { SensorDataDB } from '../../client';
-import { formatter_tooltip} from '../../utils'
+import { formatter_tooltip } from '../../utils'
+import { getLocalAvgDataApiLinigrafosAvgLocalIntervalGetOptions } from '../../client/@tanstack/react-query.gen';
+import { useQuery } from '@tanstack/react-query';
+import { Box, Heading, Skeleton } from '@chakra-ui/react';
 
-const LevelChart = ({ data }: { data: Array<SensorDataDB> }) => (
+const LevelChart = ({ location }: { location: string }) => {
+    const { data, status } = useQuery({
+        ...getLocalAvgDataApiLinigrafosAvgLocalIntervalGetOptions(
+            {
+                path: { local: location, interval: 'hourly' },
+                query: { daysago: 5 }
+            }
+        )
+    })
 
-    <ResponsiveContainer width="100%" height={200}>
-        <AreaChart
-            width={500}
-            height={200}
-            data={data}
-            syncId="levelId"
-            margin={{
-                top: 10,
-                right: 30,
-                left: 0,
-                bottom: 0,
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-                name='Data'
-                dataKey={d => d.data_leitura.getTime()}
-                domain={[data[0].data_leitura.getTime(), data[data.length - 1].data_leitura.getTime()]}
-                tickFormatter={(value, _) => moment(value).format('HH:mm')}
-                scale="time" type="number" />
-            <YAxis />
-            <Tooltip labelFormatter={(value) => moment(value).format('DD/M/Y HH:mm')} formatter={formatter_tooltip} />
-            {/* TODO: Clean data before ploting */}
-            <Area type="monotone" name="Altura" dataKey={d => d.valor_leitura} stroke="#2257A0" fill="#51AFEF" />
-        </AreaChart>
-    </ResponsiveContainer>
-);
+    if (status === 'pending') {
+        return <span>Loading...</span>
+    }
+
+    if (status === 'error' || typeof (data) === undefined) {
+        return <span>Error</span>
+    }
+
+    return (
+        <Box padding={"1rem"}>
+            <Heading size={"sm"} marginBottom={"1rem"}>
+                {location}
+            </Heading>
+            {
+                data.length == 0 ?
+                    <Skeleton height="200px" variant="none" marginX={"40px"} /> :
+                    <ResponsiveContainer width="100%" height={200}>
+                        <AreaChart
+                            width={500}
+                            height={200}
+                            data={data}
+                            syncId="levelId"
+                            margin={{
+                                top: 10,
+                                right: 30,
+                                left: 0,
+                                bottom: 0,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis
+                                name='Data'
+                                dataKey={d => d.date.getTime()}
+                                domain={[data[0].date.getTime(), data[data.length - 1].date.getTime()]}
+                                tickFormatter={(value, _) => moment(value).format('DD/M HH:mm')}
+                                scale="time" type="number" />
+                            <YAxis />
+                            <Tooltip labelFormatter={(value) => moment(value).format('DD/M/Y HH:mm')} formatter={formatter_tooltip} />
+                            {/* TODO: Clean data before ploting */}
+                            <Area type="monotone" name="Altura" dataKey={d => d.avg_height} stroke="#2257A0" fill="#51AFEF" />
+                        </AreaChart>
+                    </ResponsiveContainer>
+            }
+        </Box>
+    )
+};
 
 export default LevelChart;
--- frontend/src/routes/_layout/linigrafos.tsx
+++ frontend/src/routes/_layout/linigrafos.tsx
@@ -1,11 +1,10 @@
-import { Grid, GridItem } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react"
 import { Box, Heading, useColorModeValue } from "@chakra-ui/react"
 import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from "@tanstack/react-router"
-import moment from 'moment';
 
-import Charts from "../../components/linigrafos/Charts"
-import { getDataApiLinigrafosGetOptions } from "../../client/@tanstack/react-query.gen"
+import { getLocationsApiLocationGetOptions } from "../../client/@tanstack/react-query.gen"
+import LevelChart from "../../components/linigrafos/LevelChart";
 
 
 export const Route = createFileRoute("/_layout/linigrafos")({
@@ -16,34 +15,25 @@ function Dashboard() {
 
     const fgColor = useColorModeValue("ui.dark", "ui.light")
 
-    const { data, status } = useQuery({
-        ...getDataApiLinigrafosGetOptions({
-            query: {
-                start: moment().utc().subtract(1, 'day').startOf('day').toDate(),
-                end: moment().subtract(1, 'day').endOf('day').toDate()
-            },
-
-        }),
+    const { data: locations, status } = useQuery({
+        ...getLocationsApiLocationGetOptions({})
     })
     if (status === 'pending') {
         return <span>Loading...</span>
     }
-    if (status === 'error' || typeof (data) === undefined) {
+    if (status === 'error' || typeof (locations) === undefined) {
         return <span>Error</span>
     }
 
     return (
         <>
             <Box maxH={"100vh"} pt={12} m={4}>
-                <Heading size={"2xl"} color={fgColor}>
-                    Sensores de nivel
+                <Heading size={"2xl"} color={fgColor} margin={"2rem"}>
+                    Sensores de nivel - Últimos 5 dias
                 </Heading>
-                <Grid templateColumns="repeat(5, 1fr)" templateRows="10rem auto" gap={"2.4rem"} height={"auto"}>
-                    {/* <MetStats /> */}
-                    <GridItem gridRow={2} colSpan={4}>
-                        <Charts data={data} />
-                    </GridItem>
-                </Grid>
+                <Flex gap={"2.4rem"} height={"auto"} direction={"column"}>
+                    {locations.map(location => <LevelChart location={location.nome} />)}
+                </Flex>
             </Box>
         </>
     )
